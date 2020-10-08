99c7f94219a92c30aeefb786831014e7a70de435
Opilane TTHK <opilane@tthk.ee>
2020-10-08 11:27:23 +0300

    Week6Functions

---
+++ Params/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParamsSum(5, 5); 
+        }
+
+        public static void ParamsSum(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine(result);
+        }
+
+    }
+}
---
+++ Week6Functions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Week6Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloUser();
+            Sum();
+
+        }
+
+        public static void HelloUser()
+        {
+            Console.WriteLine("Enter your name");
+            string username = Console.ReadLine();
+            Console.WriteLine($"Hello, {username}");
+
+        }
+
+        public static void Sum()
+        {
+            Console.WriteLine("what is your first name?");
+            
+            int Num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second name");
+            int Num2 = Int32.Parse(Console.ReadLine());
+             
+
+        }
+        
+
+
+    }
+}
